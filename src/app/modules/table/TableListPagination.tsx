e28e11f4679066c15c6e19ba7c419b32d44c33c1
Ayman Bitar <ayman@missioncontrol.gg>
2022-05-17 17:07:08 -0400

    Added Scrolltotop after pagination change

--- src/app/modules/table/TableListPagination.tsx
+++ src/app/modules/table/TableListPagination.tsx
@@ -1,6 +1,7 @@
 import clsx from 'clsx'
 import {useQueryResponseLoading, useQueryResponsePagination} from "./QueryResponseProvider";
 import {useQueryRequest} from "./QueryRequestProvider";
+import {ScrollTopComponent} from "../../../_metronic/assets/ts/components";
 
 const TableListPagination = () => {
     const pagination = useQueryResponsePagination()
@@ -13,6 +14,12 @@ const TableListPagination = () => {
         }
 
         updateState({page, per_page: pagination.per_page || 10})
+
+        scrollTop()
+    }
+
+    const scrollTop = () => {
+        ScrollTopComponent.goTop()
     }
 
     const currentPage = (link: any) => {
@@ -26,30 +33,28 @@ const TableListPagination = () => {
             <div
                 className='col-sm-12 col-md-5 d-flex align-items-center justify-content-center justify-content-md-start'/>
             <div className='col-sm-12 col-md-7 d-flex align-items-center justify-content-center justify-content-md-end'>
-                <div id='kt_table_users_paginate'>
-                    <ul className='pagination'>
-                        {pagination.links?.map((link) => (
-                            <li
-                                key={link.label}
-                                className={clsx('page-item', {
-                                    active: pagination.current_page === currentPage(link),
-                                    disabled: isLoading,
-                                    previous: link.label === '&laquo; Previous',
-                                    next: link.label === 'Next &raquo;',
-                                })}
-                            >
+                <ul className='pagination'>
+                    {pagination.links?.map((link) => (
+                        <li
+                            key={link.label}
+                            className={clsx('page-item', {
+                                active: pagination.current_page === currentPage(link),
+                                disabled: isLoading,
+                                previous: link.label === '&laquo; Previous',
+                                next: link.label === 'Next &raquo;',
+                            })}
+                        >
 
-                                <a className='page-link'
-                                   onClick={() => {
-                                       updatePage(currentPage(link))
-                                   }}
-                                   dangerouslySetInnerHTML={{__html: link.label}}
-                                   style={{cursor: 'pointer'}}
-                                />
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                            <a className='page-link'
+                               onClick={() => {
+                                   updatePage(currentPage(link))
+                               }}
+                               dangerouslySetInnerHTML={{__html: link.label}}
+                               style={{cursor: 'pointer'}}
+                            />
+                        </li>
+                    ))}
+                </ul>
             </div>
         </div>
     )
