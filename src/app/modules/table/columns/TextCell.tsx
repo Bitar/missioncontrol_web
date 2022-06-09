c2fbfccbaac5dc700e5fe5a27b0c668a8206521b
Mark Mariscal <markmariscal@Marks-iMac.local>
2022-06-09 13:27:59 -0700

    Community Tab Final Bug Fixes

--- src/app/modules/table/columns/TextCell.tsx
+++ src/app/modules/table/columns/TextCell.tsx
@@ -8,7 +8,7 @@ type Props = {
 const TextCell: FC<Props> = ({dObject}) => (
     <div className='d-flex align-items-center'>
         <div className='d-flex flex-column'>
-            <a href='#' className='text-gray-800 text-hover-primary mb-1'>
+            <a href='#' className='text-gray-800 pe-none mb-1'>
                 {dObject}
             </a>
             {/*<span>{dObject.email}</span>*/}
