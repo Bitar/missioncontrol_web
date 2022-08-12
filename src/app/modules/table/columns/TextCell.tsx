66e6871312682f2d12bcc47636e61141aee14a1f
Ayman Bitar <ayman@missioncontrol.gg>
2022-08-12 15:45:48 -0400

    Updating User Index Column

--- src/app/modules/table/columns/TextCell.tsx
+++ src/app/modules/table/columns/TextCell.tsx
@@ -1,18 +1,32 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import {FC} from 'react'
+import { FC } from "react";
+import { toAbsoluteUrl } from "../../../../_metronic/helpers";
 
 type Props = {
-    dObject: any
+  dObject: any,
+  showImage?: boolean
 }
 
-const TextCell: FC<Props> = ({dObject}) => (
-    <div className='d-flex align-items-center'>
-        <div className='d-flex flex-column'>
-            <span className='text-gray-800 pe-none mb-1'>
-                {dObject}
-            </span>
-        </div>
+const TextCell: FC<Props> = ({ dObject, showImage = false }) => (
+  <>
+    <div className="d-flex align-items-center">
+      {showImage &&
+        <>
+          <div className="symbol symbol-circle symbol-50px overflow-hidden me-3">
+            <a href="../../demo1/dist/apps/user-management/users/view.html">
+              <div className="symbol-label">
+                <img src={toAbsoluteUrl("media/avatars/300-6.jpg")} alt="Emma Smith" className="w-100" />
+              </div>
+            </a>
+          </div>
+          <div className="d-flex flex-column">
+          <a href="../../demo1/dist/apps/user-management/users/view.html" className="text-gray-800 text-hover-primary mb-1">{dObject}</a>
+          <span>smith@kpmg.com</span>
+          </div>
+        </>
+      }
     </div>
-)
+  </>
+);
 
-export {TextCell}
+export { TextCell };
--- src/app/sections/identity/user/core/_columns.tsx
+++ src/app/sections/identity/user/core/_columns.tsx
@@ -1,38 +1,53 @@
-import {Column} from 'react-table'
-import {TextCell} from "../../../../modules/table/columns/TextCell";
-import {CustomHeader} from "../../../../modules/table/columns/CustomHeader";
-import {ActionsCell} from "../../../../modules/table/columns/ActionsCell";
-import {QUERIES} from "../../../../../_metronic/helpers";
-import {User} from "../../../../models/identity/User";
-import {ImageCell} from "../../../../modules/table/columns/ImageCell";
+import { Column } from "react-table";
+import { TextCell } from "../../../../modules/table/columns/TextCell";
+import { CustomHeader } from "../../../../modules/table/columns/CustomHeader";
+import { ActionsCell } from "../../../../modules/table/columns/ActionsCell";
+import { QUERIES, toAbsoluteUrl } from "../../../../../_metronic/helpers";
+import { User } from "../../../../models/identity/User";
+import { ImageCell } from "../../../../modules/table/columns/ImageCell";
+import { Link } from "react-router-dom";
 
 const usersColumns: ReadonlyArray<Column<User>> = [
   {
     Header: (props) =>
-        <CustomHeader tableProps={props} title='Name' className='min-w-125px'/>,
-    id: 'Image',
-    Cell: ({...props}) => <ImageCell dObject={props.data[props.row.index].meta.image}/>,
+      <CustomHeader tableProps={props} title="Name" className="min-w-125px" />,
+    id: "name",
+    Cell: ({ ...props }) => (
+      <>
+        <div className="d-flex align-items-center">
+          <>
+            <div className="symbol symbol-circle symbol-50px overflow-hidden me-3">
+              <Link to={`/users/${props.data[props.row.index].id}`}>
+                <div className="symbol-label">
+                  {props.data[props.row.index]?.meta?.image &&
+                    <img src={toAbsoluteUrl(props.data[props.row.index]?.meta?.image)} alt="Emma Smith" className="w-100" />
+                  }
+                </div>
+              </Link>
+            </div>
+            <div className="d-flex flex-column">
+              <Link to={`/users/${props.data[props.row.index].id}`} className="text-gray-800 text-hover-primary mb-1">{props.data[props.row.index].name}</Link>
+              <span>{props.data[props.row.index].email}</span>
+            </div>
+          </>
+        </div>
+      </>
+    )
   },
   {
     Header: (props) =>
-        <CustomHeader tableProps={props} title='Name' className='min-w-125px'/>,
-    id: 'name',
-    Cell: ({...props}) => <TextCell dObject={props.data[props.row.index].name}/>,
-  },
-  {
-    Header: (props) =>
-        <CustomHeader tableProps={props} title='Email' className='min-w-125px'/>,
-    id: 'email',
-    Cell: ({...props}) => <TextCell dObject={props.data[props.row.index].email}/>,
+      <CustomHeader tableProps={props} title="Email" className="min-w-125px" />,
+    id: "email",
+    Cell: ({ ...props }) => <TextCell dObject={props.data[props.row.index].email} />
   },
   {
     Header: (props) => (
-        <CustomHeader tableProps={props} title='Actions' className='text-end min-w-100px'/>
+      <CustomHeader tableProps={props} title="Actions" className="text-end min-w-100px" />
     ),
-    id: 'actions',
-    Cell: ({...props}) => <ActionsCell id={props.data[props.row.index].id} path={'users'}
-                                       queryKey={QUERIES.USERS_LIST} showView={true} showEdit={false}/>,
-  },
-]
+    id: "actions",
+    Cell: ({ ...props }) => <ActionsCell id={props.data[props.row.index].id} path={"users"}
+                                         queryKey={QUERIES.USERS_LIST} showView={true} showEdit={false} />
+  }
+];
 
-export {usersColumns}
+export { usersColumns };
