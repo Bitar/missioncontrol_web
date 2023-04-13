b38497078e2f01a9e7b3a2dc03a4aabcc4d0156f
Ayman Bitar <ayman@missioncontrol.gg>
2023-04-13 10:16:27 -0400

    v3.3.2

--- src/GTM.ts
+++ src/GTM.ts
@@ -1,7 +1,6 @@
 import TagManager from 'react-gtm-module'
 
 const GTM_ID = 'GTM-W594H63' // Replace with your GTM container ID.
-
 export const initializeGTM = () => {
   const tagManagerArgs = {
     gtmId: GTM_ID,
