6374a31054fe0135db84c293ae4d642c402be73d
Ayman Bitar <ayman@missioncontrol.gg>
2023-04-14 13:02:44 -0400

    Updated Dashboard cards for non-admin/community-admin

--- src/app/pages/dashboard/DashboardWrapper.tsx
+++ src/app/pages/dashboard/DashboardWrapper.tsx
@@ -1,33 +1,56 @@
-import React, {FC, useEffect} from 'react'
-import {useAuth} from '../../modules/auth'
-import {isCommunityAdmin, isSuperAdmin} from '../../models/iam/User'
-import {CommunityViewRoutes} from '../../routes/community/CommunityViewRoutes'
-import {SuperAdmin} from './partials/SuperAdmin'
-import {useMcApp} from '../../modules/general/McApp'
-import {generatePageTitle} from '../../helpers/pageTitleGenerator'
-import {Sections} from '../../helpers/sections'
-import {PageTypes} from '../../helpers/variables'
+import React, { FC, useEffect } from "react";
+import { useAuth } from "../../modules/auth";
+import { isCommunityAdmin, isSuperAdmin } from "../../models/iam/User";
+import { CommunityViewRoutes } from "../../routes/community/CommunityViewRoutes";
+import { SuperAdmin } from "./partials/SuperAdmin";
+import { useMcApp } from "../../modules/general/McApp";
+import { generatePageTitle } from "../../helpers/pageTitleGenerator";
+import { Sections } from "../../helpers/sections";
+import { PageTypes } from "../../helpers/variables";
+import { CreateCommunityWidget } from "../../layout/widgets/CreateCommunityWidget";
+import { DownloadAppWidget } from "../../layout/widgets/DownloadAppWidget";
+import { Col, Row } from "react-bootstrap";
 
 const DashboardWrapper: FC<React.PropsWithChildren<unknown>> = () => {
-  const {currentUser, communityAdmin} = useAuth()
-  const mcApp = useMcApp()
+  const { currentUser, communityAdmin } = useAuth();
+  const mcApp = useMcApp();
 
   useEffect(() => {
-    mcApp.setPageTitle(generatePageTitle(Sections.DASHBOARD, PageTypes.INDEX))
+    mcApp.setPageTitle(generatePageTitle(Sections.DASHBOARD, PageTypes.INDEX));
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    if (!currentUser) return;
+
+    console.log(isSuperAdmin(currentUser));
+    console.log(isCommunityAdmin(currentUser));
+  }, [currentUser]);
 
   return (
     <>
       {currentUser &&
-        (isSuperAdmin(currentUser) ? (
-          <SuperAdmin />
-        ) : (
-          isCommunityAdmin(currentUser) &&
-          communityAdmin && <CommunityViewRoutes communityId={communityAdmin?.id} />
-        ))}
-    </>
-  )
-}
+        (isSuperAdmin(currentUser) ? (<SuperAdmin />)
+            : (
+              (isCommunityAdmin(currentUser) && communityAdmin) ?
+                <CommunityViewRoutes communityId={communityAdmin?.id} /> : (
+                  <>
+                    <Row>
+                      <Col lg={6}>
+                        <CreateCommunityWidget />
+                      </Col>
+                      <Col lg={6}>
+                        <DownloadAppWidget />
+                      </Col>
+                    </Row>
+                  </>
 
-export {DashboardWrapper}
+                )
+            )
+        )
+      }
+    </>
+  );
+};
+
+export { DashboardWrapper };
