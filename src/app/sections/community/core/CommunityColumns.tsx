d5da39f6dab35e84fecc1c5ff40e86e2d862769b
Ayman Bitar <ayman@missioncontrol.gg>
2023-04-18 10:41:37 -0400

    Added Created on to Communities Index:

--- src/app/sections/community/core/CommunityColumns.tsx
+++ src/app/sections/community/core/CommunityColumns.tsx
@@ -1,78 +1,91 @@
-import {Column} from 'react-table'
-import {TextCell} from '../../../modules/table/columns/TextCell'
-import {CustomHeader} from '../../../modules/table/columns/CustomHeader'
-import {Community} from '../../../models/community/Community'
-import {QUERIES} from '../../../../_metronic/helpers'
-import {ActionsCell} from '../../../modules/table/columns/ActionsCell'
-import {TextImageCell} from '../../../modules/table/columns/TextImageCell'
-import React from 'react'
-import {communityStatus} from '../../../helpers/CommunityHelper'
-import {useAccessControl} from '../../../modules/auth/core/AuthPermission'
+import { Column } from "react-table";
+import { TextCell } from "../../../modules/table/columns/TextCell";
+import { CustomHeader } from "../../../modules/table/columns/CustomHeader";
+import { Community } from "../../../models/community/Community";
+import { QUERIES } from "../../../../_metronic/helpers";
+import { ActionsCell } from "../../../modules/table/columns/ActionsCell";
+import { TextImageCell } from "../../../modules/table/columns/TextImageCell";
+import React from "react";
+import { communityStatus } from "../../../helpers/CommunityHelper";
+import { useAccessControl } from "../../../modules/auth/core/AuthPermission";
+import dayjs from "dayjs";
 
 const communitiesColumns: ReadonlyArray<Column<Community>> = [
   {
-    Header: (props) => <CustomHeader tableProps={props} title='Name' className='min-w-125px' />,
-    id: 'name',
+    Header: (props) => <CustomHeader tableProps={props} title="Name" className="min-w-125px" />,
+    id: "name",
     defaultCanSort: true,
-    Cell: ({...props}) => (
+    Cell: ({ ...props }) => (
       <TextImageCell
         dImage={props.data[props.row.index].logo}
         dText={props.data[props.row.index].name}
         link={`/communities/${props.data[props.row.index].id}`}
       />
-    ),
+    )
   },
   {
-    Header: (props) => <CustomHeader tableProps={props} title='Members' className='min-w-125px' />,
-    id: 'users_count',
+    Header: (props) => <CustomHeader tableProps={props} title="Members" className="min-w-125px" />,
+    id: "users_count",
     defaultCanSort: true,
-    Cell: ({...props}) => (
+    Cell: ({ ...props }) => (
       <TextCell dObject={props.data[props.row.index].additional_data.players_count} />
-    ),
+    )
   },
   {
-    Header: (props) => <CustomHeader tableProps={props} title='Status' className='min-w-125px' />,
-    id: 'status',
+    Header: (props) => <CustomHeader tableProps={props} title="Status" className="min-w-125px" />,
+    id: "status",
     defaultCanSort: true,
-    Cell: ({...props}) => {
-      const {color, text} = communityStatus(props.data[props.row.index]?.status)
+    Cell: ({ ...props }) => {
+      const { color, text } = communityStatus(props.data[props.row.index]?.status);
 
       return (
-        <div className='d-flex align-items-center'>
-          <div className='d-flex flex-column'>
+        <div className="d-flex align-items-center">
+          <div className="d-flex flex-column">
             <span className={`badge badge-${color}`}>{text}</span>
           </div>
         </div>
-      )
-    },
+      );
+    }
   },
   {
     Header: (props) => (
-      <CustomHeader tableProps={props} title='Activities In Progress' className='min-w-125px' />
+      <CustomHeader tableProps={props} title="Activities In Progress" className="min-w-125px" />
     ),
-    id: 'activities_count',
+    id: "activities_count",
     defaultCanSort: true,
-    Cell: ({...props}) => (
+    Cell: ({ ...props }) => (
       <TextCell dObject={props.data[props.row.index].additional_data.activities_in_progress} />
-    ),
+    )
   },
   {
-    Header: (props) => <CustomHeader tableProps={props} title='Actions' className='min-w-125px' />,
-    id: 'actions',
-    Cell: ({...props}) => {
-      const accessControl = useAccessControl()
+    Header: (props) => (
+      <CustomHeader tableProps={props} title="Created On" className="min-w-125px" />
+    ),
+    id: "created_at",
+    defaultCanSort: true,
+    Cell: ({ ...props }) => (
+      <span className="text-gray-800 pe-none mb-1">
+        {new Date(props.data[props.row.index].created_at * 1000).toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' })}
+      </span>
+    )
+  },
+  {
+    Header: (props) => <CustomHeader tableProps={props} title="Actions" className="min-w-125px" />,
+    id: "actions",
+    Cell: ({ ...props }) => {
+      const accessControl = useAccessControl();
 
       return (
         <ActionsCell
           id={props.data[props.row.index].id}
-          path={'communities'}
-          showView={accessControl.userCan('view-communities')}
-          showDelete={accessControl.userCan('manage-communities')}
+          path={"communities"}
+          showView={accessControl.userCan("view-communities")}
+          showDelete={accessControl.userCan("manage-communities")}
           queryKey={QUERIES.COMMUNITIES_LIST}
         />
-      )
-    },
-  },
-]
+      );
+    }
+  }
+];
 
-export {communitiesColumns}
+export { communitiesColumns };
