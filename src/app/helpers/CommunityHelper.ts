860731ce024ed6e568a8f1aba70f45c46ff4fb8f
Ayman Bitar <ayman@missioncontrol.gg>
2023-04-24 12:57:08 -0400

    Added Community Closed status for Index

--- src/app/helpers/CommunityHelper.ts
+++ src/app/helpers/CommunityHelper.ts
@@ -1,65 +1,68 @@
 export const communityAccessType = (accessType: number) => {
-  let text = ''
-  let color = ''
+  let text = "";
+  let color = "";
 
   if (accessType === 1) {
-    color = 'success'
-    text = 'Public'
+    color = "success";
+    text = "Public";
   } else if (accessType === 2) {
-    color = 'danger'
-    text = 'Private'
+    color = "danger";
+    text = "Private";
   } else {
-    color = 'secondary'
-    text = 'Unknown'
+    color = "secondary";
+    text = "Unknown";
   }
 
-  return {color, text}
-}
+  return { color, text };
+};
 
-export const communityStatus = (accessType: number) => {
-  let text = ''
-  let color = ''
+export const communityStatus = (status: number) => {
+  let text = "";
+  let color = "";
 
-  if (accessType === 2) {
-    color = 'success'
-    text = 'Active'
+  if (status === 1) {
+    color = "secondary";
+    text = "Pending";
+  } else if (status === 2) {
+    color = "success";
+    text = "Active";
   } else {
-    color = 'secondary'
-    text = 'Pending'
+    color = "danger";
+    text = "Closed";
   }
 
-  return {color, text}
-}
+  return { color, text };
+};
 
 export const formatCommunitySubscriptionStatus = (statusId: number) => {
-  let color: string
-  let status: string
+  let color: string;
+  let status: string;
 
   if (statusId === 1) {
-    status = 'Incomplete'
-    color = 'danger'
+    status = "Incomplete";
+    color = "danger";
   } else if (statusId === 2) {
-    status = 'Incomplete Expired'
-    color = 'danger'
+    status = "Incomplete Expired";
+    color = "danger";
   } else if (statusId === 3) {
-    status = 'Trialing'
-    color = 'secondary'
+    status = "Trialing";
+    color = "secondary";
   } else if (statusId === 4) {
-    status = 'Active'
-    color = 'success'
+    status = "Active";
+    color = "success";
   } else if (statusId === 5) {
-    status = 'Past Due'
-    color = 'danger'
+    status = "Past Due";
+    color = "danger";
   } else if (statusId === 6) {
-    status = 'Canceled'
-    color = 'danger'
+    status = "Canceled";
+    color = "danger";
   } else if (statusId === 7) {
-    status = 'Unpaid'
-    color = 'danger'
+    status = "Unpaid";
+    color = "danger";
   } else {
-    status = 'Unknown'
-    color = 'danger'
+    status = "Unknown";
+    color = "danger";
   }
 
-  return {status, color}
-}
+  return { status, color };
+};
